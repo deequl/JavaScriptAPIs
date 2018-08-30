5670a8ecbbdeee6c701c674cea4c5cfce6071e04
David <dakohbod@gmail.com>
2018-08-30 19:25:48 +0200

    vulnarable-dependencies

--- 18_MemoryGameApp/memory-game/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MemoryGame from './MemoryGame';
-import './index.css';
-
-ReactDOM.render(
-  <MemoryGame />,
-  document.getElementById('root')
-);
